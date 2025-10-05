04b49a97f4663bcab1cd9c4b4bdc3cb78c39928c
ptimons44 <103891668+ptimons44@users.noreply.github.com>
2025-10-05 15:08:21 -0700

    use short circuiting program design in compiler target selection

--- src/main/java/decaf/DecafCompiler.java
+++ src/main/java/decaf/DecafCompiler.java
@@ -3,8 +3,11 @@ package decaf;
 import decaf.utils.CommandLineInterface;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.decaf.utils.CommandLineInterface.CompilerAction;
 
 public class DecafCompiler {
 
@@ -12,16 +15,30 @@ public class DecafCompiler {
         CommandLineInterface.parse(args, new String[0]);
         try (InputStream inputStream = CommandLineInterface.infile == null ? System.in : Files.newInputStream(Path.of(CommandLineInterface.infile));
              OutputStream outputStream = CommandLineInterface.outfile == null ? System.out : new PrintStream(new FileOutputStream(CommandLineInterface.outfile))) {
-            switch (CommandLineInterface.target) {
-                case SCAN -> {
-                }
-                case PARSE -> {
-                }
-                case INTER -> {
-                }
-                case ASSEMBLY -> {
-                }
+            
+            String in = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+            Scan scan = new Scan(in);
+            scan.scan();
+            if (CommandLineInterface.target == SCAN) {
+                scan.write(outputStream);
+                return 0;
             }
+
+            Parse parse = new Parse(scan);
+            parse.parse();
+            if (CommandLineInterface.target == PARSE) {
+                return parse.getErrors();
+            }
+
+            if (CommandLineInterface.target == INTER) {
+                return 0;
+            }
+
+            if (CommandLineInterface.target == ASSEMBLY) {
+                return 0;
+            }
+            return 0;
         } catch (IOException ioe) {
             System.err.printf("IOException encountered while processing file: %s", CommandLineInterface.infile);
             System.exit(1);
