ec946fe867e4d6712c9f811e149d4b4359b26773
ptimons44 <103891668+ptimons44@users.noreply.github.com>
2025-11-11 23:11:59 -0800

    seperate parsing by syntatic category

--- src/main/java/decaf/Parse.java
+++ src/main/java/decaf/Parse.java
@@ -10,14 +10,75 @@ import java.util.ArrayList;
 
 public class Parse {
     private List<LexicalToken> tokens;
-    private List<String> errors = new ArrayList<>();
+    private String error = null;
     private List<String> warnings = new ArrayList<>();
+
+    private enum SyntacticEnv {
+        DECL,
+        STMT,
+        EXPR,
+        ERROR
+    }
+
+    private SyntacticEnv getSyntacticEnv(LexicalToken token) {
+        switch (token.getTokenType()) {
+            case KEYWORD:
+                String val = token.getVal();
+                if (val.equals("class") || val.equals("void") || val.equals("int") || val.equals("boolean") || val.equals("char") || val.equals("long") || val.equals("if") || val.equals("while") || val.equals("return")) {
+                    return SyntacticEnv.DECL;
+                } else {
+                    return SyntacticEnv.STMT;
+                }
+            case IDENTIFIER:
+            case INTLITERAL:
+            case STRINGLITERAL:
+            case CHARLITERAL:
+            case BOOLEANLITERAL:
+            case LONGLITERAL:
+                return SyntacticEnv.EXPR;
+            case PUNCTUATION:
+                return SyntacticEnv.STMT;
+            default:
+                return null;
+        }
+    }
     
     public Parse(List<LexicalToken> tokens) {
+        /*
+         * Uses LL(1) parsing to parse the token stream for non-Expression grammar rules.
+         * Uses Pratt parsing to parse Expression grammar rules.
+         */
         this.tokens = tokens;
+
+        LexicalToken lookahead = null;
+        int pos = 0;
+        while (pos < tokens.size()) {
+            lookahead = tokens.get(pos);
+            SyntacticEnv syntacticEnv = getSyntacticEnv(lookahead);
+            if (syntacticEnv == SyntacticEnv.DECL) {
+                parseDecl(pos);
+            } else if (syntacticEnv == SyntacticEnv.STMT) {
+                parseStmt(pos);
+            } else if (syntacticEnv == SyntacticEnv.EXPR) {
+                parseExpr(pos, 0);
+            } else {
+                this.error = "Unknown syntactic environment for token: " + lookahead.toString();
+                return;
+            }
+
+        }
+    }
+
+    private void parseDecl(int pos) {
+    }
+
+    private void parseStmt(int pos) {
+    }
+
+    private void parseExpr(int pos, int precedence) {
     }
 
     public boolean getIsValidProgram() {
-        return this.tokens != null && this.errors.isEmpty();
+        return this.tokens != null && this.error == null;
     }
 }
--- src/main/java/decaf/types/LexicalToken.java
+++ src/main/java/decaf/types/LexicalToken.java
@@ -2,9 +2,11 @@ package decaf.types;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@ToString
 public class LexicalToken {
     public enum TokenType {
         CHARLITERAL, 
